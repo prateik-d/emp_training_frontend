129406eb79394860a5fda9e82cb7be5e7c6ad602
Prateik <pratik.dhotmal@gmail.com>
2020-03-16 09:33:06 +0530

    courses module

---
+++ src/app/trainer/courses/courses.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// import { CategoryService } from 'src/app/shares/trainer/category.service';
+import { CourseService } from 'src/app/shares/trainer/course.service';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+  courses = [];
+  constructor(
+    private router: Router,
+    private courseService: CourseService
+  ) 
+  { 
+
+    this.courseService.get_all_courses().subscribe((data) => {
+        
+      this.courses = data.result;
+
+      console.log(data);
+      
+  });
+  }
+
+
+  ngOnInit() 
+  {
+
+  }
+
+  // ngOnDestroy(): void {
+  //   // Do not forget to unsubscribe the event
+  //   this.dtTrigger.unsubscribe();
+  // }
+}
