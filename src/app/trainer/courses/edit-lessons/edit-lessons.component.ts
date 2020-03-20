e3505836b95aba6c42e09fb6ec9b8e0a117d2c54
NikunjKanani <nikunjkanani868@gmail.com>
2020-03-20 18:43:57 +0530

    edit lesson component added 2.0

---
+++ src/app/trainer/courses/edit-lessons/edit-lessons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-lessons',
+  templateUrl: './edit-lessons.component.html',
+  styleUrls: ['./edit-lessons.component.css']
+})
+export class EditLessonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
