0f14a822b56eb0db0fe51424f85c62427a06092d
Prateik <pratik.dhotmal@gmail.com>
2020-04-06 11:19:16 +0530

    environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost/vtrain/'
+  // baseUrl: 'http://localhost/vtrain/'
+  // baseUrl: 'http://13.58.242.226/emp_training/backend/vtrain/'
+  baseUrl: 'http://betatesting.in/emp_training/backend/vtrain/'
 };
